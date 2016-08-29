59eaaad691bf47aa462fe139fb073d15e2fc706b
StephenLU0422 <1029252135@qq.com>
2016-08-29 22:28:37 +0800

    HDFS2HBase ready the main()

--- src/main/java/cn/project/hbase/HDFS2HBase.java
+++ src/main/java/cn/project/hbase/HDFS2HBase.java
@@ -2,20 +2,44 @@ package cn.project.hbase;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 public class HDFS2HBase {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		//配置
+		Configuration conf = HBaseConfiguration.create();
+		conf.set(TableOutputFormat.OUTPUT_TABLE, "t2");
+		Job job = Job.getInstance(conf, HDFS2HBase.class.getSimpleName());
+		//设置jar依赖/mapreduce包
+		TableMapReduceUtil.addDependencyJars(job);
+		job.setJarByClass(HDFS2HBase.class);
+		//set Mapper/Reducer
+		job.setMapperClass(BatchImportMapper.class);
+		job.setReducerClass(BatchImportReducer.class);
+		//设置map输出，不设置reduce的
+		job.setMapOutputKeyClass(LongWritable.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		//输出格式类型，不指定输出路径
+		job.setOutputFormatClass(TableOutputFormat.class);
+		FileInputFormat.setInputPaths(job, "/hbase/t1-out/part-*");
+		job.waitForCompletion(true);
 	}
 	static class BatchImportMapper extends Mapper<LongWritable, Text, LongWritable, Text>{
 		Text v2 = new Text();
