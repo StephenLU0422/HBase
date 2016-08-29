688825aefc43d2561e9bb8e689c411943f7f3539
StephenLU0422 <1029252135@qq.com>
2016-08-29 22:14:45 +0800

    HDFS2Hbase add reducer

--- src/main/java/cn/project/hbase/HDFS2HBase.java
+++ src/main/java/cn/project/hbase/HDFS2HBase.java
@@ -2,10 +2,16 @@ package cn.project.hbase;
 
 import java.io.IOException;
 
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 public class HDFS2HBase {
 	public static void main(String[] args) {
@@ -28,8 +34,22 @@ public class HDFS2HBase {
 				Counter counter = context.getCounter("BatchImport", "ErrorFormat");
 				counter.increment(1L);
 				System.out.println("出错："+splited[0]+""+e.getMessage());
-				
-				
+			}
+		}
+	}
+	
+	static class BatchImportReducer extends TableReducer<LongWritable, Text, NullWritable>{
+		@Override
+		protected void reduce(LongWritable key, Iterable<Text> values,
+				Reducer<LongWritable, Text, NullWritable, Mutation>.Context context)
+				throws IOException, InterruptedException {
+			for (Text text : values) {
+				String[] splited = text.toString().split("\t");
+				Put put = new Put(Bytes.toBytes(splited[0]));
+				put.add(Bytes.toBytes("f1"), Bytes.toBytes("name"), Bytes.toBytes(splited[1]));
+				put.add(Bytes.toBytes("f1"), Bytes.toBytes("age"), Bytes.toBytes(splited[2]));
+				//put.add ...
+				context.write(NullWritable.get(), put);
 			}
 		}
 	}
